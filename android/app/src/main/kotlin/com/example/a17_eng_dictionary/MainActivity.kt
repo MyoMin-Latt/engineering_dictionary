4df5af1d8f27d9f3e18b27ac0b134f6341adb800
MyoMinLatt <myominlatt330@gmail.com>
2022-03-16 10:36:30 +0630

    @17.7 Connect Dictionary Database

---
+++ android/app/src/main/kotlin/com/example/a17_eng_dictionary/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.a17_eng_dictionary
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/database/dictionary_dao.dart
@@ -0,0 +1,15 @@
+import 'package:a17_eng_dictionary/database/dictionary_database.dart';
+import 'package:a17_eng_dictionary/database/dictionary_table.dart';
+import 'package:drift/drift.dart';
+part 'dictionary_dao.g.dart';
+
+@DriftAccessor(tables: [DictionaryTable])
+class DictionaryDao extends DatabaseAccessor<DictionaryDatabase>
+    with _$DictionaryDaoMixin {
+  DictionaryDao(DictionaryDatabase dictionaryDatabase)
+      : super(dictionaryDatabase);
+
+  Future<List<DictionaryTableData>> getAllWord() async {
+    return await select(dictionaryTable).get();
+  }
+}
